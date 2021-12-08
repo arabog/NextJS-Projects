0050532ed4f72961d59f29127a6d370d6585882d
iCode_Rx <arababs2015@gmail.com>
2021-12-08 11:32:25 +0100

    Nextjs official doc

---
+++ Intro/pages/posts/first-post.js
@@ -0,0 +1,21 @@
+// The component can have any name, 
+// but you must export it as a default export.
+
+import Link from "next/link"
+
+
+export default function FirstPost() {
+
+          
+          return (
+                    <>
+                              <h1>First Post</h1>
+
+                              <h2>
+                                        <Link href = "/">
+                                                  <a> Back to home </a>
+                                        </Link>
+                              </h2>
+                    </>
+          )
+}
