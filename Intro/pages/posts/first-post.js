b1fd6d6b8e367e7fdc44eed8387c9c579f81a4b0
iCode_Rx <arababs2015@gmail.com>
2021-12-11 19:56:20 +0100

    updates

--- Intro/pages/posts/first-post.js
+++ Intro/pages/posts/first-post.js
@@ -2,13 +2,20 @@
 // but you must export it as a default export.
 
 import Link from "next/link"
+import Head from "next/head"
+
+import Layout from "../../components/layout"
 
 
 export default function FirstPost() {
 
           
           return (
-                    <>
+                    <Layout>
+                              <Head>
+                                        <title>First Post</title>
+                              </Head>
+
                               <h1>First Post</h1>
 
                               <h2>
@@ -16,6 +23,6 @@ export default function FirstPost() {
                                                   <a> Back to home </a>
                                         </Link>
                               </h2>
-                    </>
+                    </Layout>
           )
 }
